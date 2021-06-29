1e5e89b5d693bd9af5d97ba2c3fcdecba5301471
dinolive666 <74519168+dinolive666@users.noreply.github.com>
2021-06-29 15:20:24 +0800

    Update relation.py

--- relation.py
+++ relation.py
@@ -1,3 +1,33 @@
+# 代码整理 找出所有循环
+
+import pandas as pd
+import networkx as nx
+import numpy as np
+import csv 
+import matplotlib.pyplot as plt 
+
+relation = pd.read_csv("./data4/relation.csv")
+
+edges = list(zip(relation.source, relation.target))
+
+# 创建有向图
+graph = nx.DiGraph()
+graph.add_edges_from(edges)
+nx.draw_kamada_kawai(graph, with_labels = True)
+
+#找出所有循环
+data = list(nx.simple_cycles(graph))
+file = open('./data4/cycles.csv' , 'w+', newline = '')
+with file:
+    write = csv.writer(file)
+    write.writerows(data)
+
+
+
+    
+    
+    
+
 # 试验networkx中关于circle的算法
 import pandas as pd
 import networkx as nx
